4b2cbbdc5a6e3aec03104c55bc6d217cb3d84cd4
Qasim <mianqasim065@gmail.com>
2025-05-11 17:13:09 +0500

    Select all the label elements

--- banking.js
+++ banking.js
@@ -19,4 +19,16 @@ const account3 = {
   pin: 3333,
 };
 
+//array of objects  
+const accounts = [account1, account2, account3];
+//elements
+const labelWelcome = document.querySelector(".Welcome");
+const lableTopDate=document.querySelector(".date");
+const lableTotalBalance = document.querySelector(".total-balance");
+const lableTransactionDate=document.querySelector(".transaction-date");
+const labelWithdrawal=document.querySelector(".withdrawal");
+const labelDeposit=document.querySelector(".deposit");
+const labelSummaryDeposite=document.querySelector(".summary-deposite");
+const labelSummaryWithdrawal=document.querySelector(".summary-withdrawal");
+
 
