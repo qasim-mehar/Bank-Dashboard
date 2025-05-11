09ac7f8d1517ac44458f5d151a4e10b5fe73769c
Qasim <mianqasim065@gmail.com>
2025-05-11 17:03:35 +0500

    Create accounts objects

---
+++ banking.js
@@ -0,0 +1,22 @@
+"strict mode"
+//user's objects
+const account1 = {
+  owner: "Jonas Schmedtmann",
+  movements: [200, 450, -400, 3000, -650, -130],
+  interestRate: 1.2,
+  pin: 1111,
+};
+const account2 = {
+  owner: "Jessica Davis",
+  movements: [5000, 3400, -150, -790, -3210, -1000],
+  interestRate: 1.5,
+  pin: 2222,
+};
+const account3 = {
+  owner: "Steven Thomas Williams",
+  movements: [200, -200, 340, -300, -20, 50],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+
